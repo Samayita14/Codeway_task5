957c23f675d777648ea78b4176c133e0ae6f5e75
Samayita Chatterjee <samayitachatterjee14@gmail.com>
2024-02-09 13:53:55 +0530

    Added the Quiz game application and modified the README file
    game.py
    	Consists all the logic and codes related to the Quiz game
    	application.
    quiz.py
    	Consists all the Questions and the set of answers that I have
    	used for this game.

---
+++ quiz game/ques.py
@@ -0,0 +1,88 @@
+questionlists= [
+    "Which type of Programming does Python support?",
+    "Which of the following is not a core data type in Python programming?",
+    "Which of these is the definition for packages in Python?",
+    "Who developed Python Programming Language?",
+    "Which of the following is the correct extension of the Python file?",
+    "All keywords in Python are in ",
+    "Which of the following is used to define a block of code in Python language?",
+    "Which keyword is used for function in Python language?",
+    "Which of the following character is used to give single-line comments in Python?",
+    " Which of the following functions can help us to find the version of python that we are currently working on?",
+]
+answerset = {
+    1:[
+        "object-oriented programming",
+        "structured programming",
+        "functional programming",
+        "all of the mentioned"
+    ],
+    2:[
+        "Tuples",
+        "Lists",
+        "Class",
+        "Dictionary"
+    ],
+    3:[
+        "A set of main modules",
+        "A folder of python modules",
+        "A number of files containing Python definitions and statements",
+        " A set of programs making use of Python modules"
+    ],
+    4:[
+        "Wick van Rossum",
+        "Rasmus Lerdorf",
+        "Guido van Rossum",
+        "Niene Stom"
+    ],
+    5:[
+        ".python",
+        ".pl",
+        ".py",
+        ".p"
+
+    ],
+    6:[
+        "Capitalized",
+        "lower case",
+        "UPPER CASE",
+        "None of the mentioned"
+    ],
+    7:[
+        "Indentation",
+        "Key",
+        "Brackets",
+        "All of the mentioned"
+    ],
+    8:[
+        "Function",
+        "def",
+        "Fun",
+        "Define"    
+    ],
+    9:[
+        "//",
+        "#",
+        "!",
+        "/*"
+    ],
+    10:[
+        "sys.version(1)",
+        "sys.version(0)",
+        "sys.version()",
+        "sys.version"
+    ],
+}
+
+correctAnswers = {
+    1:"all of the mentioned",
+    2:"Class",
+    3:"A folder of python modules",
+    4:"Guido van Rossum",
+    5:".py",
+    6:"None of the mentioned",
+    7:"Indentation",
+    8:"def",
+    9:"#",
+    10:"sys.version"
+}
